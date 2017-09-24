abfbe7f1369951655a994be3585c7555cc28fd5f
yaffaalbzour <yaffaalbzour@users.noreply.github.com>
2017-09-24 18:45:23 +0300

    Initial commit

---
+++ src/knockknockclient/Client.java
@@ -0,0 +1,33 @@
+package knockknockclient;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) throws IOException 
+    {
+        try (
+            Socket kkSocket = new Socket("localhost", 1234);
+            PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(kkSocket.getInputStream()));
+        ) {
+            BufferedReader stdIn =
+                new BufferedReader(new InputStreamReader(System.in));
+            String meaningFromServer;
+            String wordFromUser;
+
+            while ((meaningFromServer = in.readLine()) != null) {
+                System.out.print("The Word: " + meaningFromServer);
+                if (meaningFromServer.equals("Bye."))
+                    break;
+                
+                wordFromUser = stdIn.readLine();
+                if (wordFromUser != null) {
+                    System.out.println("Means: " + wordFromUser);
+                    out.println(wordFromUser);
+                }
+            }
+        }
+    }
+}
